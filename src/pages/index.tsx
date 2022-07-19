7aa413417740f1869090244f2f2a5a7888271bc0
Kleber Freire <ch4r4d4@gmail.com>
2022-07-19 16:42:57 -0300

    prettier configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const foo = "bla";
+  const foo = 'bla'
 
   return (
     <div>
       <h1>Boilerplate Kleber Freire</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
