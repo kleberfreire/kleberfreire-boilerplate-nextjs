ea223cd7bcd13f0b68ba43f5f19a8588144730bd
Kleber Freire <ch4r4d4@gmail.com>
2022-07-19 14:58:36 -0300

    nextjs created

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Boilerplate Kleber Freire</h1>
+    </div>
+  );
+};
+
+export default Home;
